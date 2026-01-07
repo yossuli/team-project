8794b5ed3fcd6a8c50efbbf54f76a9a6467075f5
Shunya <136313317+shunya-yan@users.noreply.github.com>
2026-01-07 13:23:24 +0900

    マッチングの仮作成（登録ボタンを押した後の制御）
    * initial commit
    
    * DBテーブル作成と結合（マッチング履歴、ブロックリスト、登録後のマッチング系のテーブル）
    
    * 開始時刻と終了時刻から出発時刻のみに変更
    
    * 登録の際に日付指定、マッチングアルゴリズム（仮）の作成、orsmの追加（経路が出せる）、マッチング待ちの予約表示、マッチング失敗時のアラートの追加
    
    * マッチングの仮作成

--- client/routes/__root.tsx
+++ client/routes/__root.tsx
@@ -1,8 +1,10 @@
+// 👇 [1] Clerkの認証制御コンポーネントをインポート
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { Container } from "@ss/jsx";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-// 👇 [1] Clerkの認証制御コンポーネントをインポート
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
+// 👇 [2] 追加: 監視用コンポーネントをインポート
+import { DeadlineWatcher } from "../components/DeadlineWatcher";
 import { Header } from "../components/original/Header";
 
 export const Route = createRootRoute({
@@ -15,11 +17,12 @@ function RootComponent() {
     <>
       <Header />
       <Container flexDirection="column" p="4">
-        
         {/* --- 🔒 認証による表示の切り替え --- */}
 
         {/* 1. ログインしている時だけ、ページの中身(Outlet)を表示 */}
         <SignedIn>
+          {/* 👇 [3] 追加: ここに置くことで、ログイン中のみ常に監視が走ります */}
+          <DeadlineWatcher />
           <Outlet />
         </SignedIn>
 
@@ -27,7 +30,6 @@ function RootComponent() {
         <SignedOut>
           <RedirectToSignIn />
         </SignedOut>
-
       </Container>
     </>
   );
